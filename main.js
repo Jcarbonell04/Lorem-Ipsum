dd650116e38476c7e1ea833c1c94393b9bdefd4f
Jcarbonell04 <61067221+Jcarbonell04@users.noreply.github.com>
2020-02-24 15:31:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,5 +11,15 @@ function clickHandler() {
 
     //Update source attribute of image
     document.getElementById('img1').src = "images/lorem-ipsum2.jpg";
+
+    //Update href or link
+    document.getElementById('link1').href = 'https://www.w3schools.com/';
+
+    //Update Style
+    document.getElementById('para2').style.fontFamily = 'Papyrus'; 
+    document.getElementById('para2').style.color = 'grey';
+
+    //Add a class to an element
+    document.getElementById('li2').classList.add('center');
 }
 
